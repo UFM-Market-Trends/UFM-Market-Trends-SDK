a26eff849f0eaa9aff2c80a198ed86f7586eae4f
José Alvarez C <alvarez.jo.developer@gmail.com>
2023-02-02 01:55:21 +0000

    Export PDF class

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='ufmtrends-sdk',
-    version='0.1.0',
+    version='0.1.1',
     author='Jose Alvarez',
     author_email='jose@tecuntecs.com',
     description='Testing installation of Package',
