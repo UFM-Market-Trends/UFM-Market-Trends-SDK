64812e09169d49ad776e490409f2a56da3ceebf2
José Alvarez C <alvarez.jo.developer@gmail.com>
2023-06-05 17:35:35 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='ufmtrends-sdk',
-    version='0.1.9',
+    version='0.2.0',
     author='Jose Alvarez',
     author_email='jose@tecuntecs.com',
     description='Testing installation of Package',
